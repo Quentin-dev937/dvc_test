7b5ad9622b944d9ef1f8ddaeefb67e96c6ff8a0f
QMorelle <quentin.morelle99@gmail.com>
2026-08-24 22:00:52 +0200

    First piepline and experiments

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,17 @@
 import os
 import pandas as pd
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-data_raw_path = f"data/raw/titanic.csv"
-data_out_path = f"data/processed/titanic.csv"
 
-def main(input_path, output_path):
+def main():
+    cfg = OmegaConf.load("params.yaml")
     
-    dataframe = pd.read_csv(input_path)
+    dataframe = pd.read_csv(cfg.data.path.raw)
     
-    dataframe.to_csv(output_path, index=False)
+    dataframe.to_csv(cfg.data.path.processed, index=False)
     
 
    
 if __name__ == "__main__":
-    main(input_path=data_raw_path, output_path=data_out_path)
+    main()
