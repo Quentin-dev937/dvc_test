969ddf63e50990bf8eeb61fb613bf4e2646db8fa
QMorelle <quentin.morelle99@gmail.com>
2026-08-28 20:11:39 +0200

    Adding validation.py

---
+++ src/validation.py
@@ -0,0 +1,33 @@
+import sys
+import great_expectations as gx
+import pandas as pd
+from omegaconf import OmegaConf, DictConfig
+import hydra
+from pathlib import Path
+
+@hydra.main(config_path="../conf", config_name="config", version_base=None)
+def validation(cfg: DictConfig):
+
+    # print(OmegaConf.to_yaml(cfg))
+
+    # data_path = cfg.data.path.raw
+    # ge_root_dir = cfg.gx.context_root_dir
+    data_source = cfg.gx.data_source
+    suite_name = cfg.gx.suite_name
+    asset_name = cfg.gx.asset_name
+    batch_definition_name = cfg.gx.batch_definition_name
+
+    context = gx.get_context()
+
+    batch_definition = context.data_sources.get(data_source).get_asset(asset_name).get_batch_definition(batch_definition_name)
+    batch = batch_definition.get_batch()
+    suite = context.suites.get(suite_name)
+
+    results = batch.validate(expect=suite)
+
+    print(results["success"], flush=True)
+
+
+
+if __name__ == "__main__":
+    validation()
