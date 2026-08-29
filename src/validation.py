983bf1747d4199d7d234d632d722df715a76d4f2
QMorelle <quentin.morelle99@gmail.com>
2026-08-29 17:24:29 +0200

    Updating ci.yaml

--- src/validation.py
+++ src/validation.py
@@ -5,20 +5,20 @@ from omegaconf import OmegaConf, DictConfig
 import hydra
 from pathlib import Path
 
-@hydra.main(config_path="../conf", config_name="config", version_base=None)
-def validation(cfg: DictConfig):
+#@hydra.main(config_path="../conf", config_name="config", version_base=None)
+#def validation(cfg: DictConfig):
+def validation():
 
-
-    data_source = cfg.gx.data_source
-    suite_name = cfg.gx.suite_name
-    asset_name = cfg.gx.asset_name
-    batch_definition_name = cfg.gx.batch_definition_name
+    #data_source = cfg.gx.data_source
+    #suite_name = cfg.gx.suite_name
+    #asset_name = cfg.gx.asset_name
+    #batch_definition_name = cfg.gx.batch_definition_name
 
     context = gx.get_context()
 
-    batch_definition = context.data_sources.get(data_source).get_asset(asset_name).get_batch_definition(batch_definition_name)
+    batch_definition = context.data_sources.get("titanic_datasource").get_asset("titanic_raw").get_batch_definition("titanic.csv")
     batch = batch_definition.get_batch()
-    suite = context.suites.get(suite_name)
+    suite = context.suites.get("titanic_raw_suite")
 
     results = batch.validate(expect=suite)
 
