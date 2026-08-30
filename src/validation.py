04ec7875710a638c8395cb3d22543c36141af37f
QMorelle <quentin.morelle99@gmail.com>
2026-08-30 21:25:08 +0200

    updating validation.py

--- src/validation.py
+++ src/validation.py
@@ -39,7 +39,7 @@ def validation():
 
     df = pd.read_csv(data_raw_path)
 
-    batch_parameters_dataframe = {"dataframe": df}
+    batch_parameters_dataframe = {"path": data_raw_path}
     results = validation_definition.run(batch_parameters=batch_parameters_dataframe)
 
 
